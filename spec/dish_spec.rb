05c83dab746b8c882fc0bc48d5f3f72000c5ac2d
Oliver <o.lucas@live.com>
2015-06-26 11:46:21 +0100

    another test added to increase robustness of testing

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -3,6 +3,7 @@ require 'dish'
 describe Dish do
 
   let(:dish) {Dish.new("Pepperoni", 9)}
+  let(:calzone) {Dish.new("Calzone", 0)}
 
   context 'identifying dishes' do
 
@@ -13,6 +14,11 @@ describe Dish do
     it 'dishes should have a price' do
       expect(dish.price).to eq 9
     end
+
+    it 'dishes should have a default price of 0' do
+      expect(calzone.price).to eq 0
+    end
+      
   end
 
 end 
